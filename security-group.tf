b5917516439676261e6b9e6e2cad714adea2ee6a
afro-juliano <afro.juliano@gmail.com>
2023-12-19 02:09:28 -0300

    Obrigado titia, sua linda

--- security-group.tf
+++ security-group.tf
@@ -1,35 +1,20 @@
 resource "aws_security_group" "traefik_sg" {
   name        = "traefik_sg"
   description = "Allow TLS inbound traffic"
-  revoke_rules_on_delete = true
-
-  egress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      protocol = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    },
-  ]
-
-  ingress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    },
-  ]
   vpc_id = aws_vpc.traefik_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -1,6 +1,9 @@
 # This is the vpc creation
 resource "aws_vpc" "traefik_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     name = "traefik_vpc"
   }
@@ -17,24 +20,48 @@ resource "aws_subnet" "traefik_subnet" {
   }
 }
 
-resource "aws_subnet" "traefik_subnet_az1" {
-  vpc_id                  = aws_vpc.traefik_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+resource "aws_network_acl" "traefik_acl" {
+  vpc_id = aws_vpc.traefik_vpc.id
+
+  ingress {
+    rule_no    = 100
+    action     = "allow"
+    protocol   = "tcp"
+    from_port  = 22
+    to_port    = 22
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    rule_no    = 100
+    action     = "allow"
+    protocol   = "-1"
+    from_port  = 0
+    to_port    = 0
+    cidr_block = "0.0.0.0/0"
+  }
+
+  subnet_ids = [aws_subnet.traefik_subnet.id]
 
   tags = {
-    name = "traefik_subnet_az1"
+    Name = "traefik_acl"
   }
 }
 
-resource "aws_subnet" "traefik_subnet_az2" {
-  vpc_id                  = aws_vpc.traefik_vpc.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+resource "aws_internet_gateway" "traefik_igw" {
+  vpc_id = aws_vpc.traefik_vpc.id
+}
 
-  tags = {
-    name = "traefik_subnet_az2"
+resource "aws_route_table" "traefik_route_table" {
+  vpc_id = aws_vpc.traefik_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.traefik_igw.id
   }
+}
+
+resource "aws_route_table_association" "traefik_association" {
+  subnet_id      = aws_subnet.traefik_subnet.id
+  route_table_id = aws_route_table.traefik_route_table.id
 }
