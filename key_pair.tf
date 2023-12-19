e19817a2459c7af8718444d9e21d1e46ba737da9
afro-juliano <afro.juliano@gmail.com>
2023-12-18 21:07:27 -0300

    AAAAAA

--- key_pair.tf
+++ key_pair.tf
@@ -1,13 +1,13 @@
-//resource "tls_private_key" "pk" {
-//  algorithm = "RSA"
-//  rsa_bits = 4096
-//}
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
 
 resource "aws_key_pair" "kp" {
   key_name = "traefik_key"
-  public_key = file("${path.module}/traefik_key.pub")
+  public_key = tls_private_key.pk.public_key_openssh
 
-//  provisioner "local-exec" {
-//  command = "echo '${tls_private_key.pk.private_key_pem}' > ./traefik_key.pem"
-//  }
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./traefik_key.pem"
+  }
 }
