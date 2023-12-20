11f633073b139da30fcfc813fa1b7d7172f19b9f
afro-juliano <afro.juliano@gmail.com>
2023-12-20 15:48:18 -0300

    Separate internet gateway and route in another file

---
+++ igw_route_association.tf
@@ -0,0 +1,19 @@
+# Internet Gateway
+resource "aws_internet_gateway" "traefik_igw" {
+  vpc_id = aws_vpc.traefik_vpc.id
+}
+
+# Route
+resource "aws_route_table" "traefik_route_table" {
+  vpc_id = aws_vpc.traefik_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.traefik_igw.id
+  }
+}
+# Route association
+resource "aws_route_table_association" "traefik_association" {
+  subnet_id      = aws_subnet.traefik_subnet.id
+  route_table_id = aws_route_table.traefik_route_table.id
+}
