3a11d3b46b2a2a5b759ce9ff2b658b6c27490249
afro-juliano <afro.juliano@gmail.com>
2023-12-22 12:32:33 -0300

    tried

--- Terraform files/sg_dockerSwarm.tf
+++ Terraform files/sg_dockerSwarm.tf
@@ -2,43 +2,43 @@ resource "aws_security_group" "swarm_sg" {
   name        = "swarm_sg"
   description = "Allow the nodes to comunicate with each other"
   vpc_id      = aws_vpc.traefik_vpc.id
-}
 
-ingress {
-  from_port   = 2377
-  to_port     = 2377
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 2377
+    to_port     = 2377
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  from_port   = 7946
-  to_port     = 7946
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  from_port   = 7946
-  to_port     = 7946
-  protocol    = "udp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  from_port   = 4789
-  to_port     = 4789
-  protocol    = "udp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-egress {
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-tags = {
-  name = "Swarm SG"
+  tags = {
+    name = "Swarm SG"
+  }
 }
